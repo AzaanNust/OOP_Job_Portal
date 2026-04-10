9d2537cbeb90f65f1b0a242d25ce3c90460c4469
Muhammad Azaan <mazaan.bscs25seecs@seecs.edu.pk>
2026-04-11 00:53:36 +0500

    Exceptions/BusinessException code completed

--- ResumeMatchingSystem/ResumeMatchingSystem/src/main/java/OOP/JobPortal/ResumeMatchingSystem/Exceptions/BusinessException.java
+++ ResumeMatchingSystem/ResumeMatchingSystem/src/main/java/OOP/JobPortal/ResumeMatchingSystem/Exceptions/BusinessException.java
@@ -1,4 +1,23 @@
 package OOP.JobPortal.ResumeMatchingSystem.Exceptions;
 
-public class BusinessException {
+/**
+ * BusinessException  –  Unchecked Exception for business rule violations
+ *
+ * Exception Handling:
+ *   Thrown when business rules are violated:
+ *   - Applying to the same job twice
+ *   - Trying to close a job you don't own
+ *   - Registering with an email already in use
+ *   GlobalExceptionHandler maps this to HTTP 400 Bad Request.
+ */
+public class BusinessException extends RuntimeException {
+
+    /**
+     * Creates the exception with a message describing the rule violation.
+     *
+     * @param message description of which business rule was violated
+     */
+    public BusinessException(String message) {
+        super(message);
+    }
 }
