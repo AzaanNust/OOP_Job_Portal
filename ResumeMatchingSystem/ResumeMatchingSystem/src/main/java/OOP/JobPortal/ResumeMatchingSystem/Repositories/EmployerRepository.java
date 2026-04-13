cc18ea099f7debce26d2342ee0ef1ef8397999cd
Sohan Raj Karera <arehmans685@gmail.com>
2026-04-13 14:05:36 +0500

    Repositories/EmployerRepository code completed

--- ResumeMatchingSystem/ResumeMatchingSystem/src/main/java/OOP/JobPortal/ResumeMatchingSystem/Repositories/EmployerRepository.java
+++ ResumeMatchingSystem/ResumeMatchingSystem/src/main/java/OOP/JobPortal/ResumeMatchingSystem/Repositories/EmployerRepository.java
@@ -1,4 +1,14 @@
 package OOP.JobPortal.ResumeMatchingSystem.Repositories;
 
-public interface EmployerRepository {
+import OOP.JobPortal.ResumeMatchingSystem.Entities.Employer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/** Database access for the employers table */
+@Repository
+public interface EmployerRepository extends JpaRepository<Employer, Long> {
+    Optional<Employer> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
